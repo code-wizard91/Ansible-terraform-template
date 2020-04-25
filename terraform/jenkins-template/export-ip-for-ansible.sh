b39a78bd366b017ffb80a5a15dc98580dd15397f
Mahboob Ali <alimizan@hotmail.com>
2020-04-25 23:40:15 +0100

    Update export-ip-for-ansible.sh

--- terraform/jenkins-template/export-ip-for-ansible.sh
+++ terraform/jenkins-template/export-ip-for-ansible.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "[jenkins]" > ../../ansible-template/inventory
+echo "[myvm]" > ../../ansible-template/inventory
 echo $(terraform output jenkins_ip) >> ../../ansible-template/inventory
 
 echo " " >> ../../ansible-template/inventory
 
-echo "[jenkins:vars]" >> ../../ansible-template/inventory
+echo "[myvm:vars]" >> ../../ansible-template/inventory
 echo "ansible_user=mizan" >> ../../ansible-template/inventory
 echo "ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> ../../ansible-template/inventory
 echo "ansible_ssh_private_key_file=~/.ssh/id_rsa" >> ../../ansible-template/inventory
