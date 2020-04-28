9e116f350adb77091cb69ac57830265b339df6c6
Mahboob Ali <alimizan@hotmail.com>
2020-04-29 00:05:36 +0100

    Update main.tf

--- terraform/ubuntu-vm-template/main.tf
+++ terraform/ubuntu-vm-template/main.tf
@@ -40,6 +40,7 @@ resource "azurerm_network_interface" "main" {
   name                = "vm-nic"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+  network_security_group_id = azurerm_network_security_group.main.id
 
   ip_configuration {
     name                          = "vm-ip-configuration"
